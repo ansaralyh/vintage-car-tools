35704a8492f63c06f2f8d5d1364fcb8dfb991a49
Gulbaz3032 <gulbazkachoo3032@gmail.com>
2025-07-01 12:59:10 +0500

    folders setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePageLayout from './components/home-page-layout/home-page-layout'
 
 function App() {
 
   return (
     <>
-      
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path='/' element={<HomePageLayout/>}>
+        
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
