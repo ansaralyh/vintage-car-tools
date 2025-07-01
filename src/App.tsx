e2491e310a5f7a0768e5dd370cf36cb98301710e
Ansar Ali <ansaralyh@gmail.com>
2025-07-01 13:34:58 +0500

    contact page added

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ import HomePageLayout from "./components/home-page-layout/home-page-layout";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Contact from "./pages/contact/contact";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-
+          <Route path="/contact" element={<Contact />} />
           <Route path="/" element={<HomePageLayout />}></Route>
         </Routes>
       </BrowserRouter>
