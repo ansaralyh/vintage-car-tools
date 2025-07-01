b08659619978e24c5d40c57875bbede57af9d4d8
Ansar Ali <ansaralyh@gmail.com>
2025-07-01 13:07:20 +0500

    All setup to code

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePageLayout from "./components/home-page-layout/home-page-layout";
 
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePageLayout from './components/home-page-layout/home-page-layout'
-
-import './App.css'
-import SignUp from './components/SignUp'
-
+import "./App.css";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
-
   return (
-
     <>
-    <BrowserRouter>
-    
-      <Routes>
-        <Route path='/' element={<HomePageLayout/>}>
-        
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          <Route path="/" element={<HomePageLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-
-    <SignUp />
-
-  )
+  );
 }
 
-export default App
+export default App;
